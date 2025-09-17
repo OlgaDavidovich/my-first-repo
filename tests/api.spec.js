dc2ef95d6d3fdeec13894c3b11b2863b76504f00
Sergey Davidovich <zukovich54@mail.ru>
2025-09-17 15:47:09 +0300

    changed hw12

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test';
 
+test.describe('API-тесты для Restful-booker', () => {
+
 const BASE_URL = 'https://restful-booker.herokuapp.com';
 const bookingData = {
     firstname: 'Tom',
@@ -15,61 +17,46 @@ const bookingData = {
   let authToken; 
   let bookingId;
 
-test('1. Создание бронирования (Create - POST)', async ({ request }) => {
+ test('1. Создание бронирования (Create - POST)', async ({ request }) => {
    const response = await request.post(`${BASE_URL}/booking`, {
     data: bookingData,
   });
+
   expect(response.status()).toBe(200);
   const responseBody = await response.json();
   expect(responseBody).toHaveProperty('bookingid');
-  expect(responseBody.booking.firstname).toBe(bookingData.firstname);
-  expect(responseBody.booking.lastname).toBe(bookingData.lastname);
-  expect(responseBody.booking.totalprice).toBe(bookingData.totalprice);
-  expect(responseBody.booking.depositpaid).toBe(bookingData.depositpaid);
-  expect(responseBody.booking.bookingdates.checkin).toBe(bookingData.bookingdates.checkin);
-  expect(responseBody.booking.bookingdates.checkout).toBe(bookingData.bookingdates.checkout);
-  expect(responseBody.booking.additionalneeds).toBe(bookingData.additionalneeds);
+  expect(responseBody.booking).toMatchObject(bookingData);
   bookingId = responseBody.bookingid;
   console.log("Бронирование успешно создано.  ID бронирования:", responseBody.bookingid);
  });
 
-      test('2. Получение информации о бронировании (Read - GET)', async ({ request }) => {
-        expect(bookingId).toBeDefined();
-        const response = await request.get(`${BASE_URL}/booking/${bookingId}`);
-        console.log(`Статус-код: ${response.status()}`);
-        expect(response.status()).toBe(200);
-        const responseBody = await response.json();
-        console.log('Тело ответа:', responseBody);
-        expect(responseBody.firstname).toBe(bookingData.firstname);
-        expect(responseBody.lastname).toBe(bookingData.lastname);
-        expect(responseBody.totalprice).toBe(bookingData.totalprice);
-        expect(responseBody.depositpaid).toBe(bookingData.depositpaid);
-        expect(responseBody.bookingdates.checkin).toBe(bookingData.bookingdates.checkin);
-        expect(responseBody.bookingdates.checkout).toBe(bookingData.bookingdates.checkout);
-        expect(responseBody.additionalneeds).toBe(bookingData.additionalneeds);
-      });
+  test('2. Получение информации о бронировании (Read - GET)', async ({ request }) => {
+   expect(bookingId).toBeDefined();
+   const response = await request.get(`${BASE_URL}/booking/${bookingId}`);
+   expect(response.status()).toBe(200);
+   const responseBody = await response.json();
+   expect(responseBody).toMatchObject(bookingData);
+  });
     
-test('3. Получение токена (POST)', async ({ request }) => {
-        const authResponse = await request.post(`${BASE_URL}/auth`,{
-        data: {
-          username: 'admin',
-          password: 'password123',
-        },
-});
-expect(authResponse.status()).toBe(200);
-    const authResponseBody = await authResponse.json();
-    expect(authResponseBody).toHaveProperty('token');
-    console.log(`Статус ответа: ${authResponse.status()}`); 
-    authToken = authResponseBody.token; 
-    console.log(`Токен авторизации получен: ${authToken}`);
-    expect(authToken).not.toBeNull();
-    expect(authToken).not.toBe('');
-});
+  test('3. Получение токена (POST) и обновление бронирования (Update - PUT)', async ({ request }) => {
+    const authResponse = await request.post(`${BASE_URL}/auth`,{
+      data: {
+        username: 'admin',
+        password: 'password123',
+      },
+  });
 
-  test('4. Обновление бронирования (Update - PUT)', async ({ request }) => {
+  expect(authResponse.status()).toBe(200);
+  const authResponseBody = await authResponse.json();
+  expect(authResponseBody).toHaveProperty('token');
+  authToken = authResponseBody.token; 
+  expect(authToken).not.toBeNull();
+  expect(authToken).not.toBe('');
+
     if (!authToken || !bookingId) {
       console.warn("Пропускаем тест: отсутствует токен или ID бронирования.");
       return;}
+      
     const updatedBookingData = {
       firstname: 'Bob',
       lastname: 'Brown',
@@ -88,31 +75,23 @@ expect(authResponse.status()).toBe(200);
         Cookie: `token=${authToken}`,
       },
     });
+
     expect(putResponse.status()).toBe(200);
     const putResponseBody = await putResponse.json();
-    console.log('Тело ответа:', putResponseBody);
-    expect(putResponseBody.firstname).toBe(updatedBookingData.firstname);
-    expect(putResponseBody.lastname).toBe(updatedBookingData.lastname);
-    expect(putResponseBody.totalprice).toBe(updatedBookingData.totalprice);
-    expect(putResponseBody.depositpaid).toBe(updatedBookingData.depositpaid);
-    expect(putResponseBody.bookingdates.checkin).toBe(updatedBookingData.bookingdates.checkin);
-    expect(putResponseBody.bookingdates.checkout).toBe(updatedBookingData.bookingdates.checkout);
-    expect(putResponseBody.additionalneeds).toBe(updatedBookingData.additionalneeds);
+    expect(putResponseBody).toMatchObject(updatedBookingData);
     console.log(`Бронирование с ID ${bookingId} успешно обновлено.`);
   });
 
-  test('5. Удаление бронирования (Delete - DELETE)', async ({ request }) => {
+  test('4. Удаление бронирования (Delete - DELETE) и проверка удаления брониования GET', async ({ request }) => {
       const deleteResponse = await request.delete(`${BASE_URL}/booking/${bookingId}`, {
           headers: {
         Cookie: `token=${authToken}`,
       },
     });
-      expect(deleteResponse.status()).toBe(201);
-      console.log(`Бронирование с ID ${bookingId} успешно удалено.`);
-   })
 
-   test('6. Проверка удаления брониования GET', async ({ request }) => {
+    expect(deleteResponse.status()).toBe(201);
+    console.log(`Бронирование с ID ${bookingId} успешно удалено.`);
     const checkResponse = await request.get(`${BASE_URL}/booking/${bookingId}`);
-    console.log(`Статус-код: ${checkResponse.status()}`);
     expect(checkResponse.status()).toBe(404);
-  });
+   })
+})
