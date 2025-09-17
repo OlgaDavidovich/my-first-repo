cc78574a243f9e3b1113568759c128afb2dab04f
Sergey Davidovich <zukovich54@mail.ru>
2025-09-17 14:41:35 +0300

    changed hw 11

--- tests/e2e.spec.js
+++ tests/e2e.spec.js
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
-import {LoginPage} from './pages/login.page';
-import { InventoryPage } from './pages/InventoryPage';
-import { CartPage } from './pages/cartPage';
-import { CheckoutStepOnePage } from './pages/checkoutStepOnePage';
-import { CheckoutStepTwoPage } from './pages/checkoutStepTwoPage';
-import { CheckoutCompletePage } from './pages/checkoutCompletePage';
+import {LoginPage} from '../pages/login.page';
+import { InventoryPage } from '../pages/InventoryPage';
+import { CartPage } from '../pages/cartPage';
+import { CheckoutStepOnePage } from '../pages/checkoutStepOnePage';
+import { CheckoutStepTwoPage } from '../pages/checkoutStepTwoPage';
+import { CheckoutCompletePage } from '../pages/checkoutCompletePage';
 
 test('Успешный логин', async({page}) => {
    const loginPage = new LoginPage(page); 
@@ -15,7 +15,7 @@ test('Успешный логин', async({page}) => {
    const checkoutCompletePage = new CheckoutCompletePage (page);
    await loginPage.open();
    await loginPage.login('standard_user', 'secret_sauce');
-   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html') 
    const pageTitle = await inventoryPage.getPageTitle ();
    expect(pageTitle).toBe('Products');
    await inventoryPage.addMostExpensiveItemToCart();
@@ -27,4 +27,5 @@ test('Успешный логин', async({page}) => {
    await checkoutStepTwoPage.finishCheckout();
    const completionMessage = await checkoutCompletePage.getCompletionMessage();
    expect(completionMessage).toBe('Thank you for your order!'); 
-   }   )
+   }
+)
